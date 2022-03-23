0be17bed890e4c30a498aa842bdc544949b41dc4
cnsejr2 <61973941+cnsejr2@users.noreply.github.com>
2022-03-23 11:29:22 +0900

    n1759

--- Study/src/n1759.java
+++ Study/src/n1759.java
@@ -2,12 +2,13 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class n1759 {
-	static String[] code;
-	
+	static char[] code;
+	static int L;
+	static int C;
 	
     static void comb(int[] arr, boolean[] visited, int depth, int n, int r) {
         if (r == 0) {
-            print(arr, visited, n);
+            makeArr(arr, visited, n);
             return;
         }
         
@@ -22,25 +23,47 @@ public class n1759 {
     }
 
     // 배열 출력
-    static void print(int[] arr, boolean[] visited, int n) {
+    static void makeArr(int[] arr, boolean[] visited, int n) {
+    	char[] word = new char [L];
+    	int j = 0;
         for (int i = 0; i < n; i++) {
             if (visited[i]) {
-                System.out.print(code[i] + " ");
+            	word[j++] = code[i];
             }
         }
-        System.out.println();
+        checkArr(word);
     }
-	
+	static void checkArr(char[] word) {
+		int cnt = 0; 
+		int cnt2 = 0;
+		for (int i = 0; i < word.length ; i++) {
+			if (word[i] == 'a' || word[i] == 'e' 
+        			|| word[i] == 'i'|| word[i] == 'o'|| word[i] == 'u') {
+        		cnt++;
+        	} else {
+        		cnt2++;
+        	}
+		}
+//		System.out.println(word.length);
+		if (cnt >= 1 && cnt2 >= 2) {
+//			for (int i = 0; word[i] != '\0' ; i++) {
+//				System.out.print(word[i]);
+//			}
+			System.out.println(word);
+		}
+		return;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);	
 		
-		int L = input.nextInt();
-		int C = input.nextInt();
+		L = input.nextInt();
+		C = input.nextInt();
 		input.nextLine();
-		String str = input.nextLine();
-
-		code = str.split(" ");
+		code = new char [C];
+		for (int i = 0; i < C; i++) {
+			code[i] = input.next().charAt(0);
+		}
 		Arrays.sort(code);
 //		System.out.println(code.length);
 //		for (int i = 0; i < code.length; i++) {
